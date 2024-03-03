712b44aea188b3ee069afac96b9ad62c97d70ce9
hrjha97 <jha.harsh837@gmail.com>
2024-03-03 12:12:58 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile"
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
     <Route path='/' element = {<Home />} />
     <Route path = "/signin" element = {<SignIn />}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+   <header className='h-16 w-full bg-slate-800 flex items-center shadow-md justify-between  p-3'>
+   <Link to="/">
+   <h1 className='font-bold text-xl md:text-2xl flex felx-wrap md:px-20'>
+        <span className='text-gray-400'>Dream</span>
+        <span className='text-red-300'>Home</span>
+    </h1>
+
+   </Link>
+    
+    <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 md:w-56'/>
+        <FaSearch className = "text-slate-600" />
+    </form>
+    <ul className="flex flex-wrap md:px-20">
+    <Link to= "/home">
+    <li className="text-slate-300 mx-3 hidden sm:inline hover:underline cursor-pointer">Home</li>
+    </Link>
+
+    <Link to= "/about">
+    <li className="text-slate-300 mx-3  hidden sm:inline  hover:underline cursor-pointer">About</li>
+    </Link>
+        
+    <Link to = "/sign-in">
+       <li className="text-slate-300 hover:underline cursor-pointer ">Sign In</li>
+    </Link>
+        
+    </ul>
+   </header>
+  )
+}
+
+export default Header
