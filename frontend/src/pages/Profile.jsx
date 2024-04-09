751f4804a4ddf7db869cd47b97c710dfadd43467
hrjha97 <jha.harsh837@gmail.com>
2024-04-09 13:07:22 +0530

    fixed show listing error

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -113,11 +113,11 @@ const handleLogout = async()=>{
       dispatch(logoutFailure(error.message));
     }
 }
-const handleShowListings = async()=>{
+const handleShowListings = async ()=>{
   try {
     setShowListingError(false);
-    const res = await fetch(`/api/user/listings/${currentUser._id}`)
-      const data = res.json();
+    const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
       if(data.success === false){
         setShowListingError(true);
         return;
@@ -184,44 +184,47 @@ const handleShowListings = async()=>{
       <p className='text-red-700 mt-5'>
         {showListingError ? "Error showing Listings" : ""}
       </p>
-      {userListings && userListings.length>0 &&
-      <div className='flex flex-col gap-4'>
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
 
-        <h1 className='text-center  mt-7 text-2xl font-semibold '>
-          Your Listings
-        </h1>
-      
-      
-      {userListings.map((listing)=>{
-        <div key={listing._id}
-        className='border rounded-lg p-3 flex justify-between gap-4 items-center'>
-        <Link to ={`/listing/${listing._id}`}>
-          <img src={listing.imageUrls[0]} alt='listing cover' className='h-16 w-16 object-contain
-           rounded-lg' />
-
-        </Link>
-        <Link className='flex-1 text-slate-700 font-semibold  hover:underline truncate' to ={`listing/${listing._id}`}>
-          <p>{listing.name}</p>
-        </Link>
-        <div className='flex flex-col items-center'>
-        <button className='text-red-700 uppercase'>
-          Delete
-        </button>
-        <button className='text-red-700 uppercase'>
-          Edit 
-        </button>
+              <div className='flex flex-col item-center'>
+                <button
+                  
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                
+                  <button className='text-green-700 uppercase'>Edit</button>
+               
+              </div>
+            </div>
+          ))}
         </div>
-        
-        </div>
-        
-      })}
-      </div>
-      
-      }
-      
+      )}
     </div>
-   
-  )
+  );
 }
 
 export default Profile
