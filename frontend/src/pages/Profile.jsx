6d52a1fc35afd198277a9e0377d85625b914f4bf
hrjha97 <jha.harsh837@gmail.com>
2024-04-10 13:36:51 +0530

    complete delete user listing functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -130,6 +130,23 @@ const handleShowListings = async ()=>{
     setShowListingError(true);
   }
 }
+const handleListingDelete = async(listingId)=>{
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method : 'DELETE',
+
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev)=>
+       prev.filter((listing)=> listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message)
+    }
+}
 
 
   return (
@@ -210,7 +227,7 @@ const handleShowListings = async ()=>{
 
               <div className='flex flex-col item-center'>
                 <button
-                  
+                  onClick={()=>handleListingDelete(listing._id)}
                   className='text-red-700 uppercase'
                 >
                   Delete
