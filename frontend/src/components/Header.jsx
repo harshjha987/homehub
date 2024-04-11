5e7ed648b664ad6585fe50b21835bf5b29e46042
hrjha97 <jha.harsh837@gmail.com>
2024-04-11 23:59:52 +0530

    update header search form fxnality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,28 @@
 import { FaSearch } from "react-icons/fa";
-import { Link,NavLink } from "react-router-dom";
+import { Link,useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux" 
+import { useEffect, useState } from "react";
 
 function Header() {
   const {currentUser} = useSelector((state) => state.user)
+  const [searchTerm , setSearchTerm ] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+
+  }
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  },[location.search])
   return (
    <header className='h-16 w-full bg-slate-300 flex items-center shadow-md justify-between  p-3'>
    <Link to="/">
@@ -14,9 +33,17 @@ function Header() {
 
    </Link>
     
-    <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 md:w-56'/>
-        <FaSearch className = "text-slate-900" />
+    <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <input type='text' placeholder='Search...' 
+        className='bg-transparent
+         focus:outline-none w-24 md:w-56'
+         value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+         />
+         <button>
+    <FaSearch className = "text-slate-900" />
+         </button>
+        
     </form>
     <ul className="flex flex-wrap md:px-20">
     
