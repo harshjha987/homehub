2999776fda42a1941006cc24a0c10746f5c22219
hrjha97 <jha.harsh837@gmail.com>
2024-04-11 13:32:35 +0530

    add contact landlord functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { test } from "../controllers/user.controller.js";
-import { updateUser,deleteUser,getUserListings } from "../controllers/user.controller.js";
+import { updateUser,deleteUser,getUserListings,getUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.get("/test",test)
@@ -9,5 +9,6 @@ router.get("/test",test)
 router.post("/update/:id",verifyToken,updateUser);
 router.delete("/delete/:id",verifyToken,deleteUser)
 router.get("/listings/:id",verifyToken,getUserListings);
+router.get("/:id",verifyToken,getUser)
 
 export default router;
