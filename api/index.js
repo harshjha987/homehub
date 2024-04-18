cc5a8917b0705aea3ad9cf29f7481225d810c9ad
hrjha97 <jha.harsh837@gmail.com>
2024-04-18 20:21:55 +0530

    ready for backend prod

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 
 dotenv.config();
 
@@ -23,6 +24,32 @@ app.use(cookieParser());
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000!!!");
 })
+const prodOrigins = [
+    getEnvironmentVariable('ORIGIN_1'),
+    getEnvironmentVariable('ORIGIN_2'),
+    getEnvironmentVariable('ORIGIN_3'),
+  ];
+  const devOrigin = ['http://localhost:5173'];
+  const allowedOrigins = getEnvironmentVariable('NODE_ENV') === 'production' ? prodOrigins : devOrigin;
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (getEnvironmentVariable('NODE_ENV') === 'production') {
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error(`${origin} not allowed by cors`));
+          }
+        } else {
+          callback(null, true);
+        }
+      },
+      optionsSuccessStatus: 200,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }),
+  );
+  
 
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter);
