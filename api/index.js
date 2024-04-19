86b1b186d2e3b313aa5fb5a61280b24bf2331231
hrjha97 <jha.harsh837@gmail.com>
2024-04-19 20:41:48 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from 'cookie-parser'
 import cors from 'cors';
-app.use(cors())
+
 
 
 dotenv.config();
