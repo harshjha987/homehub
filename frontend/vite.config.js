96a9d09673b8d4d3fbaea11b958ee2ab0ee4e2ef
hrjha97 <jha.harsh837@gmail.com>
2024-06-02 13:31:50 +0530

    ready for prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://homehub-mfrn.vercel.app/',
+        target:  'http://localhost:3000',
         secure: false,
       },
     },
